e6dd47b17fbbe0eab30ec67750640750ba3b7a7a
Peter Somlay <p.somlay@gmail.com>
2024-10-18 23:46:55 +0200

    2024-10-18_work

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,54 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="position-relative">
+        <div class="d-flex justify-content-center">
+        <form action="">
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon1">Átvevő</span>
+                <input type="text" class="form-control" aria-label="Átvevő" aria-describedby="basic-addon1" id="recipient" />
             </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon2">Név</span>
+                <input type="text" class="form-control" aria-label="Név" aria-describedby="basic-addon2" id="name" autocomplete="off" />
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon3">Telefonszám</span>
+                <input type="tel" class="form-control" aria-label="Telefonszám" aria-describedby="basic-addon3" id="mobil" autocomplete="off"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon4">E-mail</span>
+                <input type="email" class="form-control" aria-label="Telefonszám" aria-describedby="basic-addon4" id="email" autocomplete="off"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon5">Kerékpár Márka</span>
+                <input type="text" class="form-control" aria-label="Kerékpár Márka" aria-describedby="basic-addon5" id="mark"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon6">Kerékpár Típusa</span>
+                <input type="text" class="form-control" aria-label="Kerékpár Típusa" aria-describedby="basic-addon6" id="type"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon7">Kerékpár Színe</span>
+                <input type="text" class="form-control" aria-label="Kerékpár Színe" aria-describedby="basic-addon7" id="color"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon8">Ismertető Jegy</span>
+                <input type="text" class="form-control" aria-label="Ismertető Jegy" aria-describedby="basic-addon8" id="description"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon9">Behozatal Dátuma</span>
+                <input type="date" class="form-control" aria-label="Behozatal Dátuma" aria-describedby="basic-addon9" id="in_date"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon10">Vállalás Dátuma</span>
+                <input type="date" class="form-control" aria-label="Vállalás Dátuma" aria-describedby="basic-addon10" id="out_date"/>
+            </div>
+            <div class="input-group input-group-lg m-4 w-100">
+                <span class="input-group-text" id="basic-addon11">Hibák leírása</span>
+                <textarea name="errors" class="form-control" rows="20" cols="70" aria-label="Hibák leírása" aria-describedby="basic-addon11" id="errors"></textarea>
+            </div>
+        </form>
         </div>
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,26 +5,128 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Bikemen App') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="style.css" media="screen" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+            <header>
+                <nav class="navbar"></nav>
+                <div class="header-middle" style="text-align: center;">
+                    <div class="container">
+                        <div class="header-middle-left">
+                            <a href="">
+                                <img src="https://bikemen.cdn.shoprenter.hu/custom/bikemen/image/cache/w600h80m00/bikemen-logo-600x80.png?v=1610795655" />
+                            </a>
+                        </div>
                     </div>
-                </header>
+                </div>
+                <div class="header-categories-wrapper">
+                    <div class="container">
+                        <div class="module-category-wrapper">
+                            <div id="category" class="header-position">
+                                <div class="module-body">
+                                    <div id="category-nav">
+                                        <ul class="category_menu sf-horizontal">
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Napi Bevétel</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Munkafelvétel</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Regisztráció</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Javítások</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Keresések</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Beszerzés</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Napi Zárás</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Kassza</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Statisztikák</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Beosztás</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Emlékeztetők</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Könyvelőnek</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Tartozások</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Szerszám Igény</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text">Program Hibák</h3>
+                                                </a>
+                                            </li>
+                                            <li class="category-list">
+                                                <a href="" class="category-list-link">
+                                                    <h3 class="category-list-link-text text-danger">Kilépés</h3>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
             @endisset
 
             <!-- Page Content -->
